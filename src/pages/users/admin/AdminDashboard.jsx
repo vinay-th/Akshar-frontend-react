9c1ece74e341c4b5dba4a1dc32c7961471dce386
Vinay Thakor <vinay.th.24@gmail.com>
2024-09-29 14:43:00 +0530

    Admin dashboard changed

--- src/pages/users/admin/AdminDashboard.jsx
+++ src/pages/users/admin/AdminDashboard.jsx
@@ -3,19 +3,7 @@ import React, { useState } from 'react';
 import AdminSidebar from '../../../partials/AdminSidebar';
 import Header from '../../../partials/Header';
 import AdminWelcomeBanner from '../../../partials/dashboard/AdminWelcomeBanner';
-import FilterButton from '../../../components/DropdownFilter';
-import Datepicker from '../../../components/Datepicker';
-import DashboardCard01 from '../../../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../../../partials/dashboard/DashboardCard02';
-import DashboardCard03 from '../../../partials/dashboard/DashboardCard03';
-import DashboardCard04 from '../../../partials/dashboard/DashboardCard04';
-import DashboardCard05 from '../../../partials/dashboard/DashboardCard05';
-import DashboardCard06 from '../../../partials/dashboard/DashboardCard06';
-import DashboardCard07 from '../../../partials/dashboard/DashboardCard07';
-import DashboardCard08 from '../../../partials/dashboard/DashboardCard08';
-import DashboardCard09 from '../../../partials/dashboard/DashboardCard09';
-import DashboardCard10 from '../../../partials/dashboard/DashboardCard10';
-import DashboardCard11 from '../../../partials/dashboard/DashboardCard11';
+import AdminDashboardContainer from './AdminDashboardContainer.jsx';
 import { useSelector } from 'react-redux';
 
 function AdminDashboard() {
@@ -37,31 +25,7 @@ function AdminDashboard() {
             {/* Welcome banner */}
             <AdminWelcomeBanner />
 
-            {/* Cards */}
-            <div className="grid grid-cols-12 gap-6">
-              {/* Line chart (Acme Plus) */}
-              <DashboardCard01 />
-              {/* Line chart (Acme Advanced) */}
-              <DashboardCard02 />
-              {/* Line chart (Acme Professional) */}
-              <DashboardCard03 />
-              {/* Bar chart (Direct vs Indirect) */}
-              <DashboardCard04 />
-              {/* Line chart (Real Time Value) */}
-              <DashboardCard05 />
-              {/* Doughnut chart (Top Countries) */}
-              <DashboardCard06 />
-              {/* Table (Top Channels) */}
-              <DashboardCard07 />
-              {/* Line chart (Sales Over Time) */}
-              <DashboardCard08 />
-              {/* Stacked bar chart (Sales VS Refunds) */}
-              <DashboardCard09 />
-              {/* Card (Recent Activity) */}
-              <DashboardCard10 />
-              {/* Card (Income/Expenses) */}
-              <DashboardCard11 />
-            </div>
+            <AdminDashboardContainer />
           </div>
         </main>
       </div>
